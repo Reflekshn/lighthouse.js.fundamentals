d4af08726bc087ecd3775946f8c6c8c7cbe0cd6e
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-11-12 20:59:00 +0000

    Use condtionals to tell people about the weather

---
+++ conditionals.js
@@ -0,0 +1,62 @@
+/* const raining = false;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!"); */
+
+///////////////////////////////////////////////
+
+/* const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+} */
+
+////////////////////////////////////////////////
+
+/* const temperature = 20;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!"); */
+
+/////////////////////////////////////////////////
+
+/* const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+} */
+
+/////////////////////////////////////////////////
+
+/* var temperature = 50;
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+ */
+
+/////////////////////////////////////////////////
+
+var raining = false;
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
