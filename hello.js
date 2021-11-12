11b740fb8b1da9b7dc826341349227c2383cf108
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-11-12 22:46:45 +0000

    Use a function to greet a person by name

---
+++ hello.js
@@ -0,0 +1,21 @@
+/* const sayHello  = function () {
+  console.log("Hello, world");
+  console.log("Bye, world");
+}
+
+sayHello();
+*/
+
+/////////////////////////////////////////////
+/* 
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+ */
+
+/////////////////////////////////////////////
+
