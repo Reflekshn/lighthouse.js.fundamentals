abec0e69641854b261e5196a1ad29b53e074f23e
Jeremiah Orlewicz <reflekshn@hotmail.com>
2021-11-13 01:39:28 +0000

    A function to determine whether a number is odd or not

---
+++ odd.js
@@ -0,0 +1,8 @@
+function isOdd(num) {
+  return num % 2 != 0 ? true : false;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("14 is odd: " + isOdd(14));
+console.log("53 is odd: " + isOdd(53));
+console.log("80 is odd: " + isOdd(80));
